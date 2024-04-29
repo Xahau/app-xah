81877ab7968a9aa889f18ac2add0fa716626842d
Denis Angell <dangell@transia.co>
2024-04-29 13:53:56 +0200

    comment out unused fields

--- src/xah/fields.c
+++ src/xah/fields.c
@@ -32,22 +32,22 @@ bool is_normal_account_field(field_t *field) {
 const char *resolve_field_name(field_t *field) {
     if (field->data_type == STI_UINT16) {
         switch (field->id) {
-            case 1:
-                return "Ledger Entry Type";
+            // case 1:
+            //     return "Ledger Entry Type";
             case 2:
                 return "Transaction Type";
             case 3:
                 return "Signer Weight";
             case 4:
                 return "Transfer Fee";
-            case 16:
-                return "Version";
-            case 17:
-                return "Hook State Change Count";
-            case 18:
-                return "Hook Emit Count";
-            case 19:
-                return "Hook Execution Index";
+            // case 16:
+            //     return "Version";
+            // case 17:
+            //     return "Hook State Change Count";
+            // case 18:
+            //     return "Hook Emit Count";
+            // case 19:
+            //     return "Hook Execution Index";
             case 20:
                 return "Hook Api Version";
         }
@@ -64,34 +64,34 @@ const char *resolve_field_name(field_t *field) {
                 return "Source Tag";
             case 4:
                 return "Sequence";
-            case 5:
-                return "Previous Txn Lgr Seq";
-            case 6:
-                return "Ledger Sequence";
-            case 7:
-                return "Close Time";
-            case 8:
-                return "Parent Close Time";
-            case 9:
-                return "Signing Time";
+            // case 5:
+            //     return "Previous Txn Lgr Seq";
+            // case 6:
+            //     return "Ledger Sequence";
+            // case 7:
+            //     return "Close Time";
+            // case 8:
+            //     return "Parent Close Time";
+            // case 9:
+            //     return "Signing Time";
             case 10:
                 return "Expiration";
             case 11:
                 return "Transfer Rate";
             case 12:
                 return "Wallet Size";
-            case 13:
-                return "Owner Count";
+            // case 13:
+            //     return "Owner Count";
             case 14:
                 return "Destination Tag";
-            case 16:
-                return "High Quality In";
-            case 17:
-                return "High Quality Out";
-            case 18:
-                return "Low Quality In";
-            case 19:
-                return "Low Quality Out";
+            // case 16:
+            //     return "High Quality In";
+            // case 17:
+            //     return "High Quality Out";
+            // case 18:
+            //     return "Low Quality In";
+            // case 19:
+            //     return "Low Quality Out";
             case 20:
                 return "Quality In";
             case 21:
@@ -128,8 +128,8 @@ const char *resolve_field_name(field_t *field) {
                 return "Cancel After";
             case 37:
                 return "Finish After";
-            case 38:
-                return "Signer List ID";
+            // case 38:
+            //     return "Signer List ID";
             case 39:
                 return "Settle Delay";
             case 40:
@@ -138,28 +138,28 @@ const char *resolve_field_name(field_t *field) {
                 return "Ticket Sequence";
             case 42:
                 return "NFToken Taxon";
-            case 43:
-                return "Minted NFTokens";
-            case 44:
-                return "Burned NFTokens";
-            case 45:
-                return "Hook State Count";
-            case 46:
-                return "Emit Generation";
-            case 49:
-                return "Lock Count";
-            case 50:
-                return "First NFToken Sequence";
-            case 96:
-                return "Xahau Activation Lgr Seq";
-            case 97:
-                return "Import Sequence";
-            case 98:
-                return "Reward Time";
-            case 99:
-                return "Reward Lgr First";
-            case 100:
-                return "Reward Lgr Last";
+            // case 43:
+            //     return "Minted NFTokens";
+            // case 44:
+            //     return "Burned NFTokens";
+            // case 45:
+            //     return "Hook State Count";
+            // case 46:
+            //     return "Emit Generation";
+            // case 49:
+            //     return "Lock Count";
+            // case 50:
+            //     return "First NFToken Sequence";
+            // case 96:
+            //     return "Xahau Activation Lgr Seq";
+            // case 97:
+            //     return "Import Sequence";
+            // case 98:
+            //     return "Reward Time";
+            // case 99:
+            //     return "Reward Lgr First";
+            // case 100:
+            //     return "Reward Lgr Last";
         }
     }
 
@@ -173,76 +173,76 @@ const char *resolve_field_name(field_t *field) {
     if (field->data_type == STI_HASH256) {
         switch (field->id) {
             // 256-bit
-            case 1:
-                return "Ledger Hash";
-            case 2:
-                return "Parent Hash";
-            case 3:
-                return "Transaction Hash";
-            case 4:
-                return "Account Hash";
+            // case 1:
+            //     return "Ledger Hash";
+            // case 2:
+            //     return "Parent Hash";
+            // case 3:
+            //     return "Transaction Hash";
+            // case 4:
+            //     return "Account Hash";
             case 5:
                 return "Previous Txn ID";
-            case 6:
-                return "Ledger Index";
+            // case 6:
+            //     return "Ledger Index";
             case 7:
                 return "Wallet Locator";
-            case 8:
-                return "Root Index";
+            // case 8:
+            //     return "Root Index";
             case 9:
                 return "Account Txn ID";
             case 10:
                 return "NFToken ID";
-            case 11:
-                return "Emit Parent Txn ID";
-            case 12:
-                return "Emit Nonce";
-            case 13:
-                return "Emit Hook Hash";
-            case 16:
-                return "Book Directory";
+            // case 11:
+            //     return "Emit Parent Txn ID";
+            // case 12:
+            //     return "Emit Nonce";
+            // case 13:
+            //     return "Emit Hook Hash";
+            // case 16:
+            //     return "Book Directory";
             case 17:
                 return "Invoice ID";
             case 18:
                 return "Nickname";
-            case 19:
-                return "Amendment";
+            // case 19:
+            //     return "Amendment";
             case 20:
                 return "Hook On";
             case 21:
                 return "Digest";
             case 22:
                 return "Channel";
-            case 23:
-                return "Consensus Hash";
+            // case 23:
+            //     return "Consensus Hash";
             case 24:
                 return "Check ID";
-            case 25:
-                return "Validated Hash";
-            case 26:
-                return "Previous Page Min";
-            case 27:
-                return "Next Page Min";
+            // case 25:
+            //     return "Validated Hash";
+            // case 26:
+            //     return "Previous Page Min";
+            // case 27:
+            //     return "Next Page Min";
             case 28:
                 return "NFToken Buy Offer";
             case 29:
                 return "NFToken Sell Offer";
-            case 30:
-                return "Hook State Key";
+            // case 30:
+            //     return "Hook State Key";
             case 31:
                 return "Hook Hash";
             case 32:
                 return "Hook Namespace";
-            case 33:
-                return "Hook Set Txn ID";
+            // case 33:
+            //     return "Hook Set Txn ID";
             case 34:
                 return "Offer ID";
             case 35:
                 return "Escrow ID";
             case 36:
-                return "URI Token ID";
-            case 97:
-                return "Emitted Txn ID";
+                return "URIToken ID";
+            // case 97:
+            //     return "Emitted Txn ID";
             case 98:
                 return "Governance Marks";
             case 99:
@@ -263,34 +263,34 @@ const char *resolve_field_name(field_t *field) {
                 return "Taker Pays";
             case 5:
                 return "Taker Gets";
-            case 6:
-                return "Low Limit";
-            case 7:
-                return "HighLimit";
+            // case 6:
+            //     return "Low Limit";
+            // case 7:
+            //     return "High Limit";
             case 8:
                 return "Fee";
             case 9:
                 return "Send Max";
             case 10:
                 return "Deliver Min";
-            case 16:
-                return "Minimum Offer";
-            case 17:
-                return "Ripple Escrow";
-            case 18:
-                return "Delivered Amount";
+            // case 16:
+            //     return "Minimum Offer";
+            // case 17:
+            //     return "Ripple Escrow";
+            // case 18:
+            //     return "Delivered Amount";
             case 19:
                 return "NFToken Broker Fee";
-            case 20:
-                return "Hook Callback Fee";
-            case 21:
-                return "Locked Balance";
-            case 22:
-                return "Base Fee Drops";
-            case 23:
-                return "Reserve Base Drops";
-            case 24:
-                return "Reserve Increment Drops";
+            // case 20:
+            //     return "Hook Callback Fee";
+            // case 21:
+            //     return "Locked Balance";
+            // case 22:
+            //     return "Base Fee Drops";
+            // case 23:
+            //     return "Reserve Base Drops";
+            // case 24:
+            //     return "Reserve Increment Drops";
         }
     }
 
@@ -311,12 +311,12 @@ const char *resolve_field_name(field_t *field) {
                 return "Signature";
             case 7:
                 return "Domain";
-            case 8:
-                return "Fund Code";
-            case 9:
-                return "Remove Code";
-            case 10:
-                return "Expire Code";
+            // case 8:
+            //     return "Fund Code";
+            // case 9:
+            //     return "Remove Code";
+            // case 10:
+            //     return "Expire Code";
             case 11:
                 return "Create Code";
             case 12:
@@ -329,18 +329,18 @@ const char *resolve_field_name(field_t *field) {
                 return "Fulfillment";
             case 17:
                 return "Condition";
-            case 18:
-                return "Master Signature";
-            case 19:
-                return "UNLModify Validator";
-            case 20:
-                return "Validator To Disable";
-            case 21:
-                return "Validator To Re Enable";
-            case 22:
-                return "Hook State Data";
-            case 23:
-                return "Hook Return String";
+            // case 18:
+            //     return "Master Signature";
+            // case 19:
+            //     return "UNLModify Validator";
+            // case 20:
+            //     return "Validator To Disable";
+            // case 21:
+            //     return "Validator To Re Enable";
+            // case 22:
+            //     return "Hook State Data";
+            // case 23:
+            //     return "Hook Return String";
             case 24:
                 return "Hook Param Name";
             case 25:
@@ -353,18 +353,18 @@ const char *resolve_field_name(field_t *field) {
     if (field->data_type == STI_VECTOR256) {
         switch (field->id) {
             // vector 256
-            case 1:
-                return "Indexes";
-            case 2:
-                return "Hashes";
-            case 3:
-                return "Amendments";
+            // case 1:
+            //     return "Indexes";
+            // case 2:
+            //     return "Hashes";
+            // case 3:
+            //     return "Amendments";
             case 4:
-                return "NFTokenOffers";
+                return "NFToken Offers";
             case 5:
-                return "HookNamespaces";
+                return "Hook Namespaces";
             case 99:
-                return "URI Token IDs";
+                return "URIToken IDs";
         }
     }
 
@@ -386,8 +386,8 @@ const char *resolve_field_name(field_t *field) {
                 return "Regular Key";
             case 9:
                 return "NFToken Minter";
-            case 10:
-                return "Emit Callback";
+            // case 10:
+            //     return "Emit Callback";
             case 16:
                 return "Hook Account";
             case 99:
@@ -450,9 +450,9 @@ const char *resolve_field_name(field_t *field) {
                 // case 96:
                 //     return "Genesis Mint";
             case 91:
-                return "AmountEntry";
+                return "Amount Entry";
             case 92:
-                return "Mint URI Token";
+                return "Mint URIToken";
         }
     }
 
